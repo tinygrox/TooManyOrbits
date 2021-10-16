35f0baa364d65ee8b7289937d90a6f7079cf41b7
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-10-15 22:30:39 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- TooManyOrbits/AssemblyVersion.cs
+++ TooManyOrbits/AssemblyVersion.cs
@@ -5,4 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.1.6.2")]
+ [assembly: AssemblyFileVersion("1.1.6.3")]
+ [assembly: AssemblyVersion("1.1.6.3")]
--- TooManyOrbits/Properties/AssemblyInfo.cs
+++ TooManyOrbits/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
